1685ebb7f221acb1f6e11800db6770c4cfe86f05
Prit5656 <55257845+Prit5656@users.noreply.github.com>
2021-05-28 00:03:55 -0400

    Changed description

--- cypress/integration/e2e_test/Filter.js
+++ cypress/integration/e2e_test/Filter.js
@@ -2,26 +2,26 @@ describe('Filter', function () {
 	beforeEach(function () {
 		cy.visit('/browse')
 	})
-	it('Should able to filter by -Events: Epic MEGA Sale', function () {
+	it('Should able to filter by - Events: Epic MEGA Sale', function () {
 		cy.get('[data-testid=egs-filter-sidebar]')
 			.contains('Epic MEGA Sale')
 			.click()
 		cy.url().should('include', 'Epic%20MEGA')
 	})
-	it('Should be able to filter by -Price: Free', function () {
+	it('Should be able to filter by - Price: Free', function () {
 		cy.get('[data-testid=egs-filter-sidebar]').contains('Free').click()
 		cy.url().should('include', 'Free')
 	})
-	it('Should able to filter by -Genre: Action', function () {
+	it('Should able to filter by - Genre: Action', function () {
 		cy.get('[data-testid=egs-filter-sidebar]').contains('Action').click()
 		cy.url().should('include', 'Action') // => true
 	})
-	it('Should able to filter by -Genre: Open World', function () {
+	it('Should able to filter by - Genre: Open World', function () {
 		cy.get('[data-testid=egs-filter-sidebar]').contains('Open World').click()
 		cy.url().should('include', 'Open%20World') // => true
 		cy.get('[data-testid=egs-filter-sidebar]').contains('Open World').click()
 	})
-	it('Should able to filter by -Genre: Shooter', function () {
+	it('Should able to filter by - Genre: Shooter', function () {
 		cy.get('[data-testid=egs-filter-sidebar]').contains('Shooter').click()
 		cy.url().should('include', 'Shooter') // => true
 		cy.get('[data-testid=egs-filter-sidebar]').contains('Shooter').click()
